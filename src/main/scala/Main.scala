579fb7945a1ff971cb3f46b7b774b7cdccfc4a7e
johannes karoff <johannes@karoff.net>
2016-05-23 00:46:42 +0200

    no stackalloc

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,11 +6,10 @@ import native.stdio._
 object Main {
   def main(args: Array[String]): Unit = X.connect match {
     case Some(conn) =>
-      val wm = stackalloc[WM]
-      !wm = new WM(conn)
-      (!wm).registerKeys()
+      val wm = new WM(conn)
+      wm.registerKeys()
       while (true) {
-        (!wm).tick()
+        wm.tick()
       }
     case None => fprintf(stderr, c"Error connecting to x")
   }
